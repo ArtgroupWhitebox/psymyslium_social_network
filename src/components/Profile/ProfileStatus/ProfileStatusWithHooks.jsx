adc7f2a852f1d4d618fd99975dd488dc202cf3a6
WhiteBox <artgroupwhitebox@gmail.com>
2021-06-02 15:24:33 +0300

    add hook useEffect()

--- src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import classes from './ProfileStatus.module.css'
 
 const ProfileStatusWithHooks = (props) => {
 
-    let [editMode, setEditMode] = useState(false)
-    let [status, setStatus] = useState(props.status)
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    useEffect( 
+        () => { setStatus(props.status) }, 
+        [props.status]
+    )
 
     const activateEditMode = () => {
         setEditMode(true)
